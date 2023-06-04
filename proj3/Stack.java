298a4644aa5a93d5088f014d03642d3a9e8c8147
VicentF <58469939+VicentF@users.noreply.github.com>
2023-06-04 03:03:20 +0100

    Add files via upload

--- proj3/Stack.java
+++ proj3/Stack.java
@@ -1,51 +1,73 @@
 
+	/*@
+	predicate Node(Node t; Node n, int v) = t.next |-> n &*& t.val |-> v;
+	predicate List(Node n; list<int> elems) = n == null? (emp &*& elems == nil): Node(n,?nn,?v) &*& List(nn,?tail) &*& elems == cons(v,tail);
+	predicate StackInv(Stack l; list<int> elems) = l.head |-> ?h &*& List(h,elems);
+	predicate NonEmptyStackInv(Stack t; list<int> elems) = t.head |-> ?h &*& h != null &*& List(h, elems);
+	@*/
+
+
+
+class Node {
+	
+	Node next;
+	int val;
+
+	Node(int v, Node next)
+	//@requires true;
+	//@ensures Node(this,next,v);
+	{
+		this.next = next;
+		this.val = v;
+	}
+}
 
 public class Stack {
 	
 	Node head;
 	
-	/*
-	 * predicate Node(Node t; Node n, int v) = t.nxt |-> n &*& t.val |-> v;
-	 * predicate List(Node n;) = n == null ? emp : Node(n, ?nn, _) &*& List(nn);
-	 * predicate StackInv(Stack t;) = t.head |-> ?h &*& List(h);
-	 */
-	
 	public Stack() 
-	// requires @true
-	// ensures StackInv(this)
+	//@ requires true;
+	//@ ensures StackInv(this, nil);
 	{
 		this.head = null;
-    }
-
+	}
+ 
     public boolean isEmpty() 
-    //@ requires StackInv(this);
-    //@ ensures result?StackInv(this):NonEmptyStackInv(this);
+    //@ requires StackInv(this, ?l);
+    //@ ensures result?StackInv(this, ?l2):NonEmptyStackInv(this, ?l2);
     {
 		return head == null;
     }
 
     public void push(int newVal) 
+    //@requires StackInv(this, ?l);
+    //@ensures NonEmptyStackInv(this, cons(?v, ?t));
+    // ensures NonEmptyStackInv(this, ?l) &*& StackInv(this, ?);
     {
     	Node newNode = new Node(newVal, head);
     	head = newNode;
     }
 
     // l != 0 * p -> MList(p) } pop p { result == l[0] * p -> MList(l[1..]) }
+    // old(head) == l(0)
     public int pop() 
-    //@ requires NonEmptyStackInv(this);
-    //@ ensures StackInv(this);
+    //@ requires NonEmptyStackInv(this, cons(?v, ?t));
+    //@ ensures StackInv(this, t) &*& result == v;
+    // requires NonEmptyStackInv(this, ?elems);
+    // ensures StackInv(this, tail(elems)) &*& result == head(elems);
     {
-    	int val = head.getValue(); 
-    	head = head.getNext(); 
+    	int val = head.val; 
+    	head = head.next; 
     	return val;
     }
 
     // { l != 0 * p -> MList (l) } peek p { result == l[0] * p -> MList(l) }
     public int peek() 
-    //@ requires NonEmptyStackInv(this);
-    //@ ensures StackInv(this);
+    //@ requires NonEmptyStackInv(this, ?l);
+    //@ ensures StackInv(this, ?l2);
     {
-		return head.getValue();
+		return head.val;
     }
 
     public void flip() 
@@ -61,10 +83,4 @@ public class Stack {
          }
          head = prev;
     }
-
-    /**
-     * p -> MList (l)
-     * l = [0, 1, 2, 3 ...]
-     */
 }
-
